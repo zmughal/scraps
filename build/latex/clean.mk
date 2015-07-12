08ea05a96aa397a0c921567e644e4166cc5faae6
Zakariyya Mughal <zaki.mughal@gmail.com>
2015-07-12 07:29:06 -0500

    clean up the LaTeX Makefile

--- build/latex/clean.mk
+++ build/latex/clean.mk
@@ -1,6 +1,5 @@
 clean:
-	#-rm -f *.out *.log *.aux *.toc *.bbl *.blg *.nav *.snm *.vrb *.tpt *.lof *.lot *.fdb_latexmk *.fls
-	find -regex '.*\.\(out\|log\|aux\|toc\|bbl\|blg\|nav\|snm\|vrb\|tpt\|lof\|lot\|fdb_latexmk\|fls\)$$' -delete -printf "removed '%p'\n"
+	find . -regex '.*\.\(out\|log\|aux\|toc\|bbl\|blg\|nav\|snm\|vrb\|tpt\|lof\|lot\|fdb_latexmk\|fls\)$$' -delete -printf "removed '%p'\n"
 cleanall: clean
 	-rm -vf *.pdf *.dvi
 
