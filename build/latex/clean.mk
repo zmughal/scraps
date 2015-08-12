88db4cfe82ae563ebe51c849694c5d44933285a0
Zakariyya Mughal <zaki.mughal@gmail.com>
2015-08-12 11:19:09 -0500

    make the TeX clean rules double-colon rules so that they can be added to
    This allows files that include this file to add more recipes

--- build/latex/clean.mk
+++ build/latex/clean.mk
@@ -1,6 +1,6 @@
-clean:
+clean::
 	find . -regex '.*\.\(out\|log\|aux\|toc\|bbl\|blg\|nav\|snm\|vrb\|tpt\|lof\|lot\|fdb_latexmk\|fls\)$$' -delete -printf "removed '%p'\n"
-cleanall: clean
+cleanall:: clean
 	-rm -vf *.pdf *.dvi
 
 .PHONY: clean cleanall
